0b0141cf23d84b60ad5877e3fb3722200c571e77
김민석 <minseok@gimminseog-ui-MacBookPro.local>
2020-09-01 15:17:37 +0900

    2020-09-01 15:17

---
+++ ch08/src/main/java/config/AppCtx.java
@@ -0,0 +1,69 @@
+package config;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import spring.*;
+
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class AppCtx {
+
+    @Bean(destroyMethod = "close")
+    public DataSource dataSource(){
+        DataSource ds = new DataSource();
+        ds.setDriverClassName("com.mysql.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost/spring5fs?characterEncoding=utf8&useSSL=false");
+        ds.setUsername("spring5");
+        ds.setPassword("spring5");
+        ds.setInitialSize(2);
+        ds.setMaxActive(10);
+        return ds;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(){
+        DataSourceTransactionManager tm = new DataSourceTransactionManager();
+        tm.setDataSource(dataSource());
+        return tm;
+    }
+
+    @Bean
+    public MemberDao memberDao(){
+        return new MemberDao(dataSource());
+    }
+
+    @Bean
+    public MemberRegisterService memberRegSvc(){
+        return new MemberRegisterService(memberDao());
+    }
+
+    @Bean
+    public ChangePasswordService changePwdSvc(){
+        ChangePasswordService pwdSvc = new ChangePasswordService();
+        pwdSvc.setMemberDao(memberDao());
+        return pwdSvc;
+    }
+
+    @Bean
+    public MemberPrinter memberPrinter(){
+        return new MemberPrinter();
+    }
+
+    @Bean
+    public MemberListPrinter listPrinter(){
+        return new MemberListPrinter(memberDao(), memberPrinter());
+    }
+
+    @Bean
+    public MemberInfoPrinter infoPrinter(){
+        MemberInfoPrinter infoPrinter = new MemberInfoPrinter();
+        infoPrinter.setMemberDao(memberDao());
+        infoPrinter.setPrinter(memberPrinter());
+        return infoPrinter;
+    }
+}
